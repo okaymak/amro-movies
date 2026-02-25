5a7e0ed5714aeb2e3d71bf8e207c5521eb314170
Özcan Kaymak <o.kaymak@gmail.com>
2026-02-25 17:20:41 +0100

    Implement trending movies filtering by genre

---
+++ app/src/main/kotlin/com/amro/movies/domain/usecase/FilterMoviesUseCase.kt
@@ -0,0 +1,29 @@
+package com.amro.movies.domain.usecase
+
+import com.amro.movies.domain.model.Movie
+import dev.zacsweers.metro.Inject
+
+/**
+ * A use case that filters a list of movies by their associated genres.
+ */
+@Inject
+class FilterMoviesUseCase {
+
+    /**
+     * Filters the given list of movies.
+     *
+     * If [genreIds] is empty, the original list is returned. Otherwise, only movies
+     * that contain at least one of the specified genre IDs are returned.
+     *
+     * @param movies The list of movies to filter.
+     * @param genreIds The IDs of the genres to filter by.
+     * @return A filtered list of movies.
+     */
+    operator fun invoke(movies: List<Movie>, genreIds: Set<Int>): List<Movie> {
+        if (genreIds.isEmpty()) return movies
+
+        return movies.filter { movie ->
+            movie.genres.any { genre -> genre.id in genreIds }
+        }
+    }
+}
